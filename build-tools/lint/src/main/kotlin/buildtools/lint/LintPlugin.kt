404d143f6ab3575d1662c25ecfc75dca858c9072
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-02 18:57:57 +0100

    chore: setup empty LintPlugin

---
+++ build-tools/lint/src/main/kotlin/buildtools/lint/LintPlugin.kt
@@ -0,0 +1,11 @@
+package buildtools.lint
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LintPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
