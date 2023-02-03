796e7fb004bef45ef1546c84813820bc58be75de
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-03 05:08:02 +0100

    chore: setup Detekt from LintPlugin with relative rule-set

--- build-tools/lint/src/main/kotlin/buildtools/lint/LintPlugin.kt
+++ build-tools/lint/src/main/kotlin/buildtools/lint/LintPlugin.kt
@@ -1,11 +1,35 @@
 package buildtools.lint
 
+import buildtools.lint.util.apply
+import buildtools.lint.util.withType
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+@Suppress("unused")
 class LintPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        TODO("Not yet implemented")
+        target.configureDetekt()
     }
+}
+
+private fun Project.configureDetekt() {
+    allprojects {
+        apply<DetektPlugin>()
+    }
+
+    subprojects {
+
+        tasks.withType<Detekt> {
+            basePath = rootDir.path
+            config.setFrom("${rootDir.path}/detekt/config.yml")
+        }
+    }
+
+    dependencies.add(
+        "detektPlugins",
+        "com.twitter.compose.rules:detekt:0.0.26" // TODO: #1 Use from version Catalog
+    )
 }
