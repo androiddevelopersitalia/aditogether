ce4e207667ff456c13dadda99110eae37f0d805f
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-03 18:15:50 +0100

    chore: optimize Detekt setup using version catalogs

--- build-tools/lint/src/main/kotlin/aditogether/buildtools/util/PluginUtils.kt
+++ build-tools/lint/src/main/kotlin/aditogether/buildtools/util/PluginUtils.kt
@@ -3,7 +3,12 @@ package aditogether.buildtools.util
 import org.gradle.api.DomainObjectCollection
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.UnknownDomainObjectException
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.api.plugins.PluginAware
+import org.gradle.api.reflect.TypeOf
 
 /**
  * Applies the plugin of the given type [P]. Does nothing if the plugin has already been applied.
@@ -31,3 +36,28 @@ inline fun <reified P : Plugin<*>> Project.apply() {
 inline fun <reified S : Any> DomainObjectCollection<in S>.withType(
     noinline configuration: S.() -> Unit
 ): DomainObjectCollection<S> = withType(S::class.java, configuration)
+
+/**
+ * Looks for the extension of a given type.
+ *
+ * @param T the extension type
+ * @return the extension
+ * @throws UnknownDomainObjectException when no matching extension can be found
+ *
+ * @see [ExtensionContainer.getByType]
+ * @since 5.0
+ */
+inline fun <reified T : Any> ExtensionContainer.getByType(): T =
+    getByType(typeOf<T>())
+
+/**
+ * Creates an instance of [TypeOf] for the given parameterized type.
+ *
+ * @param T the type
+ * @return the [TypeOf] that captures the generic type of the given parameterized type
+ */
+inline fun <reified T> typeOf(): TypeOf<T> =
+    object : TypeOf<T>() {}
+
+val Project.libsCatalog: VersionCatalog
+    get() = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
