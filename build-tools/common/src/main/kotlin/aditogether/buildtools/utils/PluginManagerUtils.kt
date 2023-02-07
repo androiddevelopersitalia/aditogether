2e6d1a69e7d09bbde48c2ffba4311253b933d67c
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-02-07 18:46:13 +0100

    chore: apply Detekt in non-JVM Kotlin modules

--- build-tools/common/src/main/kotlin/aditogether/buildtools/utils/PluginManagerUtils.kt
+++ build-tools/common/src/main/kotlin/aditogether/buildtools/utils/PluginManagerUtils.kt
@@ -1,6 +1,7 @@
 package aditogether.buildtools.utils
 
 import org.gradle.api.Plugin
+import org.gradle.api.plugins.AppliedPlugin
 import org.gradle.api.plugins.PluginManager
 
 /**
@@ -8,4 +9,17 @@ import org.gradle.api.plugins.PluginManager
  */
 inline fun <reified P : Plugin<*>> PluginManager.apply() {
     apply(P::class.java)
+}
+
+/**
+ * Invokes [PluginManager.withPlugin] for each plugin in the inputs.
+ * This is useful to apply the same action for a list of plugins.
+ */
+fun PluginManager.withPlugins(
+    first: String,
+    vararg others: String,
+    action: (AppliedPlugin) -> Unit
+) {
+    withPlugin(first, action)
+    others.forEach { plugin -> withPlugin(plugin, action) }
 }
