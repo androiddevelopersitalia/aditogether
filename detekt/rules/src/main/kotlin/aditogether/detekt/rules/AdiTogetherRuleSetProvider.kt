77e354097e452e76bc96dee34447421dc8e5e22f
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-08 07:28:41 +0100

    chore(detekt): define AdiTogetherRuleSetProvider.kt

---
+++ detekt/rules/src/main/kotlin/aditogether/detekt/rules/AdiTogetherRuleSetProvider.kt
@@ -0,0 +1,15 @@
+package aditogether.detekt.rules
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
+class AdiTogetherRuleSetProvider : RuleSetProvider {
+
+    override val ruleSetId: String = "AdiTogether"
+
+    override fun instance(config: Config) = RuleSet(
+        ruleSetId,
+        listOf()
+    )
+}
