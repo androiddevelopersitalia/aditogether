775f46f192c79b3247bb12647b3933d849447cf0
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-02-10 22:35:18 +0100

    chore: skip tuner on Windows

--- scripts/preconditions.sh
+++ scripts/preconditions.sh
@@ -10,7 +10,15 @@ source "$SCRIPT_DIR/common/common.sh"
 P_TAG="preconditions"
 
 main() {
+  check_os
   check_symlinks_resolver
+}
+
+check_os() {
+  if [[ "$OSTYPE" != "darwin"* && "$OSTYPE" != "linux"* ]]; then
+    print_error $P_TAG "tuner is available only on Linux and macOS"
+    exit 1
+  fi
 }
 
 check_symlinks_resolver() {
