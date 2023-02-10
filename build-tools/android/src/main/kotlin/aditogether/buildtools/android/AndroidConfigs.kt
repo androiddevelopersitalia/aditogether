2fac8a985c4e21170ce0ab56cbd989d58d0355fd
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-02-10 21:45:50 +0100

    chore: add base Android plugin

---
+++ build-tools/android/src/main/kotlin/aditogether/buildtools/android/AndroidConfigs.kt
@@ -0,0 +1,11 @@
+package aditogether.buildtools.android
+
+/**
+ * Defines the shared configuration for Android targets.
+ */
+object AndroidConfigs {
+    const val COMPILE_SDK: Int = 33
+    const val MIN_SDK: Int = 23
+    const val TARGET_SDK: Int = 33
+    const val BUILD_TOOLS: String = "33.0.3"
+}
