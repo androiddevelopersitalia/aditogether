e767b44bb63f9aeeb5502ba337969d5bd09670d9
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-02-18 12:37:03 +0100

    chore: move commit-msg regex to another file to share it

--- scripts/hooks/commit-msg.sh
+++ scripts/hooks/commit-msg.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 #
-# Git commit-msg hook used to enforce the same commit convention (https://www.conventionalcommits.org/).
-#
-# Examples of valid commit messages:
-# - chore: this is the message
-# - feat(news): this is the message
-# - refactor(news)!: this is the message
-#
-# Examples on invalid commit messages:
-# - chore:
-# - fix(): this is the message
-# - docs: This is the message
-# - random_keyword: this is the message
+# Git commit-msg hook which enforces our commit convention.
+# More info can be found in `scripts/common/commit-msg-regex.sh`.
 #
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -21,12 +11,18 @@ else
   SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 fi
 source "$SCRIPT_DIR/../common/common.sh"
+source "$SCRIPT_DIR/../common/commit-msg-regex.sh"
 
 P_TAG="commit-msg hook"
 
 COMMIT_MSG_FILE=$1
 
 main() {
+  if [[ -z "$COMMIT_MSG_REGEX" ]]; then
+    print_error "$P_TAG" "COMMIT_MSG_REGEX env var missing"
+    exit 1
+  fi
+
   local lines_count
   lines_count=$(grep -c ^ "$COMMIT_MSG_FILE")
   if [[ $lines_count -gt 1 ]]; then
@@ -34,8 +30,7 @@ main() {
     exit 1
   fi
 
-  local pattern="^(build|chore|ci|docs|feat|fix|perf|refactor|style|test)+(\([a-z0-9\-_\/]+\))?!?:\s[a-z][a-zA-Z0-9 -_\/\`]+$"
-  if ! grep -q -E "$pattern" "$COMMIT_MSG_FILE"; then
+  if ! grep -q -E "$COMMIT_MSG_REGEX" "$COMMIT_MSG_FILE"; then
     print_error "$P_TAG" "the commit message does not follow https://www.conventionalcommits.org/"
     exit 1
   fi
