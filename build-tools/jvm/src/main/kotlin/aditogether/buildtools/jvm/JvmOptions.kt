ab323deffe186dacdb156cd32afd44508a5979b3
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2023-02-22 22:34:29 +0100

    chore: remove JvmOptions and useless dependencies

--- build-tools/jvm/src/main/kotlin/aditogether/buildtools/jvm/JvmOptions.kt
+++
@@ -1,9 +0,0 @@
-package aditogether.buildtools.jvm
-
-/**
- * Defines the shared configuration for targets compiled with Java (e.g. JVM, Android, KMM).
- */
-object JvmOptions {
-    const val WARNINGS_AS_ERRORS: Boolean = true
-    const val JAVA_VERSION: Int = 11
-}
